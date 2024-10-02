e8d2f6f0c145223a55f8d57e91310683b701208e
ZeQipe <zeqipe@vk.com>
2024-10-02 14:47:08 +0300

    create Posts model

--- core/apps/posts/models.py
+++ core/apps/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from apps.users.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    id = models.CharField(max_length=40, primary_key=True)
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    post_name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    users_liked = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    type_content = models.TextField()
+    url = models.TextField(unique=True)
+    tags_list = models.JSONField(default=list)
+    aspect_ratio = models.TextField(blank=True, null=True)
+    link = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
