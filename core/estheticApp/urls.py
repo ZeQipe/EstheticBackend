920ae8bb0d381a53c0c0d8dc717a96f7dac863ec
ZeQipe <zeqipe@vk.com>
2024-10-02 10:52:45 +0300

    settings server added

--- core/estheticApp/urls.py
+++ core/estheticApp/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('api.urls.url')),
+    path('', include('api.urls.urlsPosts')),
+    path('', include('api.urls.urlsUsers'))
 ]
+
+# Добавление маршрутов для медиафайлов в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
