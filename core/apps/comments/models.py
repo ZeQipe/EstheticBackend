c522810dc90722bc08500edcf0d11b27b9c7f6ed
ZeQipe <zeqipe@vk.com>
2024-10-02 15:01:11 +0300

    create Comments modell

--- core/apps/comments/models.py
+++ core/apps/comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from apps.posts.models import Post
+from apps.users.models import User
 
-# Create your models here.
+
+class Comment(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    text = models.CharField(max_length=200)
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    users_liked = models.ManyToManyField(User, related_name='liked_comments', blank=True)
