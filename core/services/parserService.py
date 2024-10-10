a6da09e990b4ac170fd656c6cc0d5a3414da0e8d
ZeQipe <zeqipe@vk.com>
2024-10-10 11:58:24 +0300

    tag parsing function created

--- core/services/parserService.py
+++ core/services/parserService.py
@@ -2,4 +2,34 @@ from services.encriptionService import Encriptions
 
 
 class Separement:
-    pass
+    @staticmethod
+    def packing_tags(tags: list) -> list:
+        if not tags:
+            return tags
+
+        id_list = []
+        while len(id_list) < len(tags):
+            id = Encriptions.generate_string(8, False)
+            if id not in id_list:
+                id_list.append(Encriptions.generate_string(8, False))
+            
+        tag_response = []
+        for i in range(len(id_list)):
+            tag_dict = {"tagId" : id_list[i],
+                        "label" : tags[i]}
+            
+            tag_response.append(tag_dict)
+        
+        return tag_response
+
+    @staticmethod
+    def unpacking_tags(tags: list[dict]) -> list[str]:
+        print(tags, type(tags))
+        if not tags:
+            return []
+        
+        prew_tags = []
+        for i in tags:
+            prew_tags.append(i["label"])
+        
+        return prew_tags
