febfeed743869f62270570ee2269a637d31b2034
ZeQipe <zeqipe@vk.com>
2024-10-15 08:33:41 +0300

    create delete data in DB service

---
+++ core/services/delService.py
@@ -0,0 +1,36 @@
+from .authService import Authorization
+from templates.answer import answer_dict as message
+from apps.comments.models import Comment
+from apps.dashboards.models import Board
+from apps.posts.models import Post
+from apps.users.models import User
+
+
+class DeletterObject:
+    @staticmethod
+    def del_object(request, model, targetID=None):
+        cookie_user = Authorization.check_logining(request)
+        
+        if isinstance(cookie_user, dict):
+            return message[401]
+        
+        elif model == User:
+            cookie_user.delete()
+        
+        else:
+            try:
+                target = model.objects.get(id=targetID)
+                
+            except Exception as er:
+                return message[400]
+        
+            if cookie_user.id != target.author.id:
+                return message[403]
+            
+            if model == Board:
+                if target.name == "Избранное":
+                    return message[403]
+
+            target.delete()
+            
+        return message[200]
