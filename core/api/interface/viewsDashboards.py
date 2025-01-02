df2874a5bcadce0d53d436712620b6cf0d98cfa8
ZeQipe <zeqipe@vk.com>
2025-01-02 20:15:31 +0300

    fetch: in file viewsDashboards add log Excepting

--- core/api/interface/viewsDashboards.py
+++ core/api/interface/viewsDashboards.py
@@ -13,7 +13,11 @@ def dashboards(request):
     # Получение досок по кукам
     elif request.method == "GET": response = get_boards_user_by_cookie(request)
 
-    else: response = message[405]
+    else: 
+        response = message[405]
+        LogException.write_data("Не существующий метод", "16", "viewsDashboards", "Не верный метод", "dashboards", "info", request,
+                                "dashboards/", f"запрос с методом - {request.method}", "405")
+        
     return JsonResponse(response, status=response.get("status", 200))
 
 
@@ -22,7 +26,11 @@ def post_in_boards(request):
     # Наличие поста в досках пользователя
     if request.method == "GET": response = check_post_in_boards(request)
 
-    else: response = message[405]
+    else: 
+        response = message[405]
+        LogException.write_data("Не существующий метод", "29", "viewsDashboards", "Не верный метод", "post_in_boards", "info", request,
+                                "dashboards/check-posts", f"запрос с методом - {request.method}", "405")
+        
     return JsonResponse(response, status=response.get("status", 200))
 
 
@@ -31,7 +39,11 @@ def dashboards_delete_posts(request, boardID):
     # Удаление поста из доски
     if request.method == "DELETE": response = remove_posts_in_board(request, boardID)
 
-    else: response = message[405]
+    else: 
+        response = message[405]
+        LogException.write_data("Не существующий метод", "42", "viewsDashboards", "Не верный метод", "dashboards_delete_posts", "info", request,
+                                "dashboards/<str:boardID>/delete-posts", f"запрос с методом - {request.method}", "405")
+        
     return JsonResponse(response, status=response.get("status", 200))
 
 
@@ -40,7 +52,11 @@ def dashboards_list(request, userID):
     # Вернуть информацию о досках
     if request.method == "GET": response = get_user_dashboards(request, userID)
 
-    else: response = message[405]
+    else: 
+        response = message[405]
+        LogException.write_data("Не существующий метод", "55", "viewsDashboards", "Не верный метод", "dashboards_list", "info", request,
+                                "dashboards/<str:userID>/list", f"запрос с методом - {request.method}", "405")
+        
     return JsonResponse(response, status=response.get("status", 200))
 
 
@@ -55,5 +71,10 @@ def dashboards_param(request, boardID):
     # Удаление доски
     elif request.method == "DELETE": response = DeletterObject.del_object(request, Board, boardID)              
 
-    else: response = message[405]
+    else: 
+        response = message[405]
+        LogException.write_data("Не существующий метод", "74", "viewsDashboards", "Не верный метод", "dashboards_param", "info", request,
+                                "dashboards/<str:boardID>", f"запрос с методом - {request.method}", "405")
+
+
     return JsonResponse(response, status=response.get("status", 200))
